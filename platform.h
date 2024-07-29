190991eb35875f157eb66ad2b623904ad9b03902
insane/rabenauge <insane.atari@gmail.com>
2024-07-29 19:27:15 +0200

    added platform.h fallback

--- platform.h
+++ platform.h
@@ -1,21 +1,34 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+// 64bit fseek+ftell macros
+
 #ifdef __MINGW32__
 #define OS_FSEEK fseeko64
 #define OS_FTELL ftello64
 #endif
+
 #ifdef __INSANEOS__
 #define OS_FSEEK fseek
 #define OS_FTELL ftell
 #endif
+
 #ifdef __linux__
 #define OS_FSEEK fseeko
 #define OS_FTELL ftello
 #endif
+
 #ifdef __GENODE__
 #define OS_FSEEK fseeko
 #define OS_FTELL ftello
+#endif
+
+// fallback
+#ifndef OS_FSEEK
+#define OS_FSEEK fseek
+#endif
+#ifndef OS_FTELL
+#define OS_FTELL ftell
 #endif
 
 #endif
